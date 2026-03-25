72bb69672d445629e21be4c6e03cf4f4b7c23528
Danilo Toriy <randomlittlest@gmail.com>
2026-03-24 21:40:59 -0300

    feat: adicionando o repositório de Fornecedor

---
+++ src/main/java/com/example/danilo/repository/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package com.example.danilo.repository;
+
+import com.example.danilo.entity.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+}
